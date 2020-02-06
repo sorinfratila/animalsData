e2f51a8123c1fa45861be82ecf35ee87e5ce22f6
Sorin Fratila <sorin@mini.local>
2020-02-06 14:30:13 +0100

    my thoughts

--- assignment.js
+++ assignment.js
@@ -1,30 +1,33 @@
-let domReady = (function () {
-
+let domReady = (function() {
   const domAnimal = {
-    titleA: '.animal_name',
-    imageA: '.animal_image-container',
-    paragraphA: '.animal_paragraph',
-    paragraphAC: '.animal_paragraph-continuation'
+    titleA: ".animal_name",
+    imageA: ".animal_image-container",
+    paragraphA: ".animal_paragraph",
+    paragraphAC: ".animal_paragraph-continuation"
   };
 
   let animalArr = animals_data.category;
 
   const manipulateTemp = {
-    valueSet: function (classHTML, valueData) {
+    valueSet: function(classHTML, valueData) {
       document.querySelector(classHTML).textContent = valueData;
     },
-    cloneTemp: function (tempSelector) {
+    cloneTemp: function(tempSelector) {
       let temp = document.querySelector(tempSelector).content;
       return temp.cloneNode(true);
     },
-    cloneSecondary: function (cloneSec, animal) {
+    cloneSecondary: function(cloneSec, animal) {
       cloneSec.querySelector(domAnimal.titleA).textContent = animal.name;
       cloneSec.querySelector(domAnimal.imageA).dataset.img1 = animal.image1;
-      cloneSec.querySelector(domAnimal.imageA).style.backgroundImage = "url(\"" + animal.image1 + "\")";
+      cloneSec.querySelector(domAnimal.imageA).style.backgroundImage =
+        'url("' + animal.image1 + '")';
       cloneSec.querySelector(domAnimal.imageA).dataset.img2 = animal.image2;
       let description = animal.description;
-      cloneSec.querySelector(domAnimal.paragraphA).textContent = description.substring(0, 160) + "...";
-      cloneSec.querySelector(domAnimal.paragraphAC).textContent = description.substring(160, description.length);
+      cloneSec.querySelector(domAnimal.paragraphA).textContent =
+        description.substring(0, 160) + "...";
+      cloneSec.querySelector(
+        domAnimal.paragraphAC
+      ).textContent = description.substring(160, description.length);
       return cloneSec;
     }
   };
@@ -38,27 +41,25 @@ let domReady = (function () {
       let cloneSec = manipulateTemp.cloneTemp("#animal");
       cloneSec = manipulateTemp.cloneSecondary(cloneSec, animal);
       clone.querySelector(".animal-class_container").appendChild(cloneSec);
-    })
+    });
 
     document.querySelector(".main_animals-container").appendChild(clone);
-  })
+  });
 
   return 1;
 })();
 
 if (domReady == true) {
-
-
-  let eventController = (function () {
+  let eventController = (function() {
     let assignEvent = {
-      assignClick: function (selector, func) {
+      assignClick: function(selector, func) {
         let array = document.querySelectorAll(selector);
 
         array.forEach(el => {
           el.addEventListener("click", func);
-        })
+        });
       }
-    }
+    };
 
     return assignEvent;
   })();
@@ -69,14 +70,19 @@ if (domReady == true) {
     }
   }
 
-  assignEvents([".animal_paragraph", ".animal_left-arrow", ".animal_right-arrow"], [seeMore, moveLeft, moveRight]);
+  assignEvents(
+    [".animal_paragraph", ".animal_left-arrow", ".animal_right-arrow"],
+    [seeMore, moveLeft, moveRight]
+  );
 
   function seeMore() {
     if (event.target.dataset.trigger === "false") {
       event.target.dataset.trigger = "true";
       let text1 = event.target.textContent;
       text1 = text1.substring(0, 160);
-      let text2 = event.target.parentElement.querySelector(".animal_paragraph-continuation").textContent;
+      let text2 = event.target.parentElement.querySelector(
+        ".animal_paragraph-continuation"
+      ).textContent;
       event.target.textContent = text1 + text2;
     } else {
       event.target.dataset.trigger = "false";
@@ -86,12 +92,16 @@ if (domReady == true) {
   }
 
   function moveLeft() {
-    let image = event.target.parentElement.querySelector(".animal_image-container");
-    image.style.backgroundImage = "url(\"" + image.dataset.img1 + "\")";
+    let image = event.target.parentElement.querySelector(
+      ".animal_image-container"
+    );
+    image.style.backgroundImage = 'url("' + image.dataset.img1 + '")'; // you can write single quotation inside double quotations, no need to escape double quotations
   }
 
   function moveRight() {
-    let image = event.target.parentElement.querySelector(".animal_image-container");
-    image.style.backgroundImage = "url(\"" + image.dataset.img2 + "\")";
+    let image = event.target.parentElement.querySelector(
+      ".animal_image-container"
+    );
+    image.style.backgroundImage = 'url("' + image.dataset.img2 + '")';
   }
 }
